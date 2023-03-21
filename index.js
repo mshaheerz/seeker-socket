075935ce9f2afb6e6cd6aabb147728792d377035
mshaheerz <mshaheerkp9@gmail.com>
2023-03-21 10:26:36 +0530

    fixes

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const io= require('socket.io')(8800,{
     cors:{
-        origin: "http://localhost:3000"
+         
     }
 })
 
@@ -13,7 +13,7 @@ io.on("connection", (socket)=> {
     socket.on('new-user-add', (newUserId) => {
         if(!activeUsers.some((user)=> user.userId === newUserId)){
             activeUsers.push({userId:newUserId,socketId: socket.id});
-            console.log('New user connected', activeUsers)
+            // console.log('New user connected', activeUsers)
         }
        
         io.emit('get-users', activeUsers)
@@ -21,16 +21,11 @@ io.on("connection", (socket)=> {
 
     
 
-    socket.on('error', function (err) { 
-        console.log("Socket.IO Error"); 
-        console.log(err.stack); // this is changed from your code in last comment
-     });
-    // send message
-
+  
     
     socket.on("disconnect", ()=> {
         activeUsers = activeUsers.filter((user)=> user.socketId !== socket.id);
-        console.log("user Disconnected", activeUsers)
+        // console.log("user Disconnected", activeUsers)
         io.emit('get-users', activeUsers)
     });
 
@@ -38,21 +33,21 @@ io.on("connection", (socket)=> {
         const { recieverId } = data;
         const user = activeUsers.find((user)=> user.userId === recieverId)
         if(user){
-            console.log('receeeeeeeeeeeeeeeeeeeev')
+            // console.log('receeeeeeeeeeeeeeeeeeeev')
             io.to(user.socketId).emit("recieve-message", data)
         }
     });
 
     socket.on("send-notification", (data) => {
         const { recieverId } = data;
-        console.log(recieverId)
+        // console.log(recieverId)
         const user = activeUsers.find(user => {
                return user.userId ===recieverId;
                
         })
-        console.log(user)
+        // console.log(user)
         if(user) {
-            console.log({data});
+            // console.log({data});
             io.to(user.socketId).emit('recieve-notification', data)
 
         }
